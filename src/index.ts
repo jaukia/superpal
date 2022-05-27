d8c87f4e7e0ca69aaa9811fa0b7ec3aa010eecca
Janne Aukia <janne@robocorp.com>
2022-05-27 22:20:13 +0300

    Fixed lint errors

--- src/index.ts
+++ src/index.ts
@@ -32,36 +32,33 @@ interface HueInformation {
  * SUPEPAL MAIN FUNCTIONS
  *************************/
 
-export const superPal = (hexColor:string, colorSpace:string=DEFAULT_COLOR_SPACE, maxHueShiftAmount:number=DEFAULT_MAX_HUE_SHIFT_AMOUNT):ColorPalette => {
+export const superPal = (hexColorIn:string, colorSpace:string=DEFAULT_COLOR_SPACE, maxHueShiftAmount:number=DEFAULT_MAX_HUE_SHIFT_AMOUNT):ColorPalette => {
   
-  let correctColorSpaceHSLColor = hexToHSL(hexColor, colorSpace);
+  const correctColorSpaceHSLColor = hexToHSL(hexColorIn, colorSpace);
   
-  let rawHSLspaceColor = hexToHSL(hexColor, "HSL");
+  const rawHSLspaceColor = hexToHSL(hexColorIn, "HSL");
   const rawHSLspaceHues = createHueLookupArray(12)(rawHSLspaceColor[0]);
 
-  let output:ColorPalette = {};
+  const output:ColorPalette = {};
 
-  let grayScaleBaseColor = HSLtoHex([correctColorSpaceHSLColor[0], 8.0, 50.0], colorSpace);
+  const grayScaleBaseColor = HSLtoHex([correctColorSpaceHSLColor[0], 8.0, 50.0], colorSpace);
   output.gray = superPalColorScale(grayScaleBaseColor, colorSpace, maxHueShiftAmount);
   
-  rawHSLspaceHues.forEach(function(rawHSLspaceHue) {
+  rawHSLspaceHues.forEach((rawHSLspaceHue) => {
 
-    let curHueName = hueName(rawHSLspaceHue);
+    const curHueName = hueName(rawHSLspaceHue);
     if(curHueName === undefined) return;
     
-    let rawHSLSpaceHexForCurHue = HSLtoHex([rawHSLspaceHue, rawHSLspaceColor[1], rawHSLspaceColor[2]], "HSL");
+    const rawHSLSpaceHexForCurHue = HSLtoHex([rawHSLspaceHue, rawHSLspaceColor[1], rawHSLspaceColor[2]], "HSL");
     
-    let curHueAngleInCorrectColorSpace = hexToHSL(rawHSLSpaceHexForCurHue, colorSpace)[0];
-    //console.log("hue angles", rawHSLspaceHue, curHueAngleInCorrectColorSpace);
+    const curHueAngleInCorrectColorSpace = hexToHSL(rawHSLSpaceHexForCurHue, colorSpace)[0];
 
     // we pass in color with equal lightness/saturation here for scale
     // generation. perhaps not the "correct" lightness, but doesn't matter
     // since the lightness values get mangled anyhow in color creation
-    let hexColor = HSLtoHex([curHueAngleInCorrectColorSpace, correctColorSpaceHSLColor[1], correctColorSpaceHSLColor[2]], colorSpace);
-
-    //console.log(hexColor, [curHueAngleInCorrectColorSpace, correctColorSpaceHSLColor[1], correctColorSpaceHSLColor[2]]);
+    const hexColor = HSLtoHex([curHueAngleInCorrectColorSpace, correctColorSpaceHSLColor[1], correctColorSpaceHSLColor[2]], colorSpace);
     
-    let colorScaleArray = superPalColorScale(hexColor, colorSpace, maxHueShiftAmount);
+    const colorScaleArray = superPalColorScale(hexColor, colorSpace, maxHueShiftAmount);
     output[curHueName] = colorScaleArray;
   });
 
@@ -72,44 +69,39 @@ export const superPal = (hexColor:string, colorSpace:string=DEFAULT_COLOR_SPACE,
 // and not based on palx
 export const superPalColorScale = (hexColor:string, colorSpace:string, maxHueShiftAmount:number):ColorScale => {
 
-  let [inHue, inSat, inLig] = hexToHSL(hexColor, colorSpace);
+  const [inHue, inSat, inLig] = hexToHSL(hexColor, colorSpace);
 
-  let okhslColor = hexToHSL(hexColor, "Okhsl");
-  let okhslHueAngle = okhslColor[0];
+  const okhslColor = hexToHSL(hexColor, "Okhsl");
+  const okhslHueAngle = okhslColor[0];
   
-  let minLightness = Math.min(...LIG_STEPS);
-  let maxLightness = Math.max(...LIG_STEPS);
+  const minLightness = Math.min(...LIG_STEPS);
+  const maxLightness = Math.max(...LIG_STEPS);
   
   // A simple heuristic for adjusting the saturation to take into account the input
   // color saturation. This may need improving at some point.
-  let satAdjustment = Math.min(1.0,inSat/SAT_STEPS[0]);
+  const satAdjustment = Math.min(1.0,inSat/SAT_STEPS[0]);
 
-  let colorMap:ColorScale = {};
+  const colorMap:ColorScale = {};
   
-  for(var i=0;i<10;i++) {
-      let curLig = LIG_STEPS[i];
-    
+  LIG_STEPS.forEach((curLig, index) => {
+      
       // recalculating color as okhsl to get correct angles, since not sure
       // what format it is really in, this would be simpler
       // if/when we can always assume okhsl color space
-      
-      let curHueRotation = rotateHue(okhslHueAngle, curLig, minLightness, maxLightness, maxHueShiftAmount);
+      const curHueRotation = rotateHue(okhslHueAngle, curLig, minLightness, maxLightness, maxHueShiftAmount);
       
       // this is not quite correct, since we are taking a delta angle from 
       // okhsl color space and applying it potentially to something else, but
       // perhaps that is not a major issue
-      let curHue = inHue + curHueRotation;
+      const curHue = inHue + curHueRotation;
 
-      //curHue = (curHue % 360);
-      //if(curHue < 0) curHue += 360.0;
-
-      let curSat = satAdjustment*SAT_STEPS[i];
-      let curColorString = Math.round(curHue) + "," + Math.round(curSat)+ "," + curLig;
+      const curSat = satAdjustment*SAT_STEPS[index];
+      const curColorString = Math.round(curHue) + "," + Math.round(curSat)+ "," + curLig;
     
-      let outHexColor = HSLtoHex([curHue, curSat, curLig], colorSpace);
+      const outHexColor = HSLtoHex([curHue, curSat, curLig], colorSpace);
     
-      colorMap[i*100] = outHexColor;
-  }
+      colorMap[index*100] = outHexColor;
+  });
   
   return colorMap;
 };
@@ -119,7 +111,7 @@ export const superPalColorScale = (hexColor:string, colorSpace:string, maxHueShi
  *************************/
 
 const HSLtoHex = (colorArray:number[], colorSpaceIn:string) => {
-  let [curHue, curSat, curLig] = colorArray;
+  const [curHue, curSat, curLig] = colorArray;
 
   let outHexColor;
   
@@ -135,14 +127,16 @@ const HSLtoHex = (colorArray:number[], colorSpaceIn:string) => {
 }
 
 const hexToHSL = (hexColor:string, colorSpaceIn:string) => {
-  let inHue, inSat, inLig;
+  let inHue;
+  let inSat;
+  let inLig;
   if(colorSpaceIn === "Okhsl") {
-    let raw = OKHSL_CONVERTER(hexColor);
+    const raw = OKHSL_CONVERTER(hexColor);
     [inHue, inSat, inLig] = [raw.h, raw.s*100.0, raw.l*100.0];
   } else if(colorSpaceIn === "HSLuv") {
     [inHue, inSat, inLig] = hexToHsluv(hexColor);
   } else if(colorSpaceIn === "HSL") {
-    let raw = HSL_CONVERTER(hexColor);
+    const raw = HSL_CONVERTER(hexColor);
     [inHue, inSat, inLig] = [raw.h, raw.s*100.0, raw.l*100.0];
   }
 
@@ -151,8 +145,8 @@ const hexToHSL = (hexColor:string, colorSpaceIn:string) => {
 
 const rotateHue = (okhslHue:number, lightness:number, minLightness:number, maxLightness:number, maxHueShiftAmount:number) => {
 
-  let lightnessMidpoint = (maxLightness-minLightness);
-  let mpDistance = Math.abs(lightness-lightnessMidpoint);
+  const lightnessMidpoint = (maxLightness-minLightness);
+  const mpDistance = Math.abs(lightness-lightnessMidpoint);
 
   let rotation;
 
@@ -167,7 +161,7 @@ const rotateHue = (okhslHue:number, lightness:number, minLightness:number, maxLi
   // https://github.com/vasturiano/d3-force-3d
   
   if(lightness>lightnessMidpoint) {
-    let {dir, distance, maxDistance} = findNearestHue(okhslHue, "light");
+    const {dir, distance, maxDistance} = findNearestHue(okhslHue, "light");
 
     let hueFractionInScale = easeInQuad(mpDistance/(maxLightness-lightnessMidpoint+15.0));
     
@@ -176,7 +170,7 @@ const rotateHue = (okhslHue:number, lightness:number, minLightness:number, maxLi
   
     rotation = dir*Math.min(distance, maxHueShiftAmount)*hueFractionInScale;      
   } else {
-    let {dir, distance, maxDistance} = findNearestHue(okhslHue, "dark");
+    const {dir, distance, maxDistance} = findNearestHue(okhslHue, "dark");
 
     let hueFractionInScale = easeOutCubic(mpDistance/(lightnessMidpoint-minLightness+40.0));
 
@@ -210,49 +204,49 @@ const findNearestHue = (okhslHue:number, mode:"light"|"dark"):HueInformation => 
       };
 
       const maxDistances = {
-        "magentaRed": (360.0+okhslColorStops["red"])-okhslColorStops["magenta"],
-        "redYellow": okhslColorStops["yellow"]-okhslColorStops["red"],
-        "yellowGreen": okhslColorStops["green"]-okhslColorStops["yellow"],
-        "greenCyan": okhslColorStops["cyan"]-okhslColorStops["green"],
-        "cyanBlue": okhslColorStops["blue"]-okhslColorStops["cyan"],
-        "blueMagenta": okhslColorStops["magenta"]-okhslColorStops["blue"]
+        "magentaRed": (360.0+okhslColorStops.red)-okhslColorStops.magenta,
+        "redYellow": okhslColorStops.yellow-okhslColorStops.red,
+        "yellowGreen": okhslColorStops.green-okhslColorStops.yellow,
+        "greenCyan": okhslColorStops.cyan-okhslColorStops.green,
+        "cyanBlue": okhslColorStops.blue-okhslColorStops.cyan,
+        "blueMagenta": okhslColorStops.magenta-okhslColorStops.blue
       };
 
-      if(hue < okhslColorStops["red"]) {
+      if(hue < okhslColorStops.red) {
         // magenta-red -> magenta
         dir = -1;
-        distance = Math.abs(okhslColorStops["magenta"]-360.0-hue);
-        maxDistance = maxDistances["magentaRed"];
-      } else if(hue < okhslColorStops["yellow"]) {
+        distance = Math.abs(okhslColorStops.magenta-360.0-hue);
+        maxDistance = maxDistances.magentaRed;
+      } else if(hue < okhslColorStops.yellow) {
         // red-yellow -> yellow
         dir = +1;
-        distance = Math.abs(okhslColorStops["yellow"]-hue);
-        maxDistance = maxDistances["redYellow"];
-      } else if(hue < okhslColorStops["green"]) {
+        distance = Math.abs(okhslColorStops.yellow-hue);
+        maxDistance = maxDistances.redYellow;
+      } else if(hue < okhslColorStops.green) {
         // yellow-green -> yellow
         dir = -1;
-        distance = Math.abs(okhslColorStops["yellow"]-hue);
-        maxDistance = maxDistances["yellowGreen"];
-      } else if(hue < okhslColorStops["cyan"]) {
+        distance = Math.abs(okhslColorStops.yellow-hue);
+        maxDistance = maxDistances.yellowGreen;
+      } else if(hue < okhslColorStops.cyan) {
         // green-cyan -> cyan
         dir = +1;
-        distance = Math.abs(okhslColorStops["cyan"]-hue);
-        maxDistance = maxDistances["greenCyan"];
-      } else if(hue < okhslColorStops["blue"]) {
+        distance = Math.abs(okhslColorStops.cyan-hue);
+        maxDistance = maxDistances.greenCyan;
+      } else if(hue < okhslColorStops.blue) {
         // cyan-blue -> cyan
         dir = -1;
-        distance = Math.abs(okhslColorStops["cyan"]-hue);
-        maxDistance = maxDistances["cyanBlue"];
-      } else if(hue < okhslColorStops["magenta"]) {
+        distance = Math.abs(okhslColorStops.cyan-hue);
+        maxDistance = maxDistances.cyanBlue;
+      } else if(hue < okhslColorStops.magenta) {
         // blue-magenta -> magenta
         dir = +1;
-        distance = Math.abs(okhslColorStops["magenta"]-hue);
-        maxDistance = maxDistances["blueMagenta"];
+        distance = Math.abs(okhslColorStops.magenta-hue);
+        maxDistance = maxDistances.blueMagenta;
       } else {
         // magenta-red -> magenta (cont)
         dir = -1;
-        distance = Math.abs(okhslColorStops["magenta"]-hue);
-        maxDistance = maxDistances["magentaRed"];
+        distance = Math.abs(okhslColorStops.magenta-hue);
+        maxDistance = maxDistances.magentaRed;
       }
       
   } else if(mode==="dark") {
@@ -267,54 +261,54 @@ const findNearestHue = (okhslHue:number, mode:"light"|"dark"):HueInformation => 
       };
 
       const maxDistances = {
-        "magentaRed": (360.0+okhslColorStops["red"])-okhslColorStops["magenta"],
-        "redYellow": okhslColorStops["yellow"]-okhslColorStops["red"],
-        "yellowGreen": okhslColorStops["green"]-okhslColorStops["yellow"],
-        "greenCyan": okhslColorStops["cyan"]-okhslColorStops["green"],
-        "cyanBlue": okhslColorStops["blue"]-okhslColorStops["cyan"],
-        "blueMagenta": okhslColorStops["magenta"]-okhslColorStops["blue"]
+        "magentaRed": (360.0+okhslColorStops.red)-okhslColorStops.magenta,
+        "redYellow": okhslColorStops.yellow-okhslColorStops.red,
+        "yellowGreen": okhslColorStops.green-okhslColorStops.yellow,
+        "greenCyan": okhslColorStops.cyan-okhslColorStops.green,
+        "cyanBlue": okhslColorStops.blue-okhslColorStops.cyan,
+        "blueMagenta": okhslColorStops.magenta-okhslColorStops.blue
       };
 
-      if(hue < okhslColorStops["red"]) {
+      if(hue < okhslColorStops.red) {
         // magenta-red -> magenta
         dir = +1;
-        distance = Math.abs(okhslColorStops["red"]-hue);
-        maxDistance = maxDistances["magentaRed"];
-      } else if(hue < okhslColorStops["yellow"]) {
+        distance = Math.abs(okhslColorStops.red-hue);
+        maxDistance = maxDistances.magentaRed;
+      } else if(hue < okhslColorStops.yellow) {
         // red-yellow -> yellow
         dir = -1;
-        distance = Math.abs(okhslColorStops["red"]-hue);
-        maxDistance = maxDistances["redYellow"];
-      } else if(hue < okhslColorStops["green"]) {
+        distance = Math.abs(okhslColorStops.red-hue);
+        maxDistance = maxDistances.redYellow;
+      } else if(hue < okhslColorStops.green) {
         // yellow-green -> yellow
         dir = +1;
-        distance = Math.abs(okhslColorStops["green"]-hue);
-        maxDistance = maxDistances["yellowGreen"];
-      } else if(hue < okhslColorStops["cyan"]) {
+        distance = Math.abs(okhslColorStops.green-hue);
+        maxDistance = maxDistances.yellowGreen;
+      } else if(hue < okhslColorStops.cyan) {
         // green-cyan -> cyan
         dir = -1;
-        distance = Math.abs(okhslColorStops["green"]-hue);
-        maxDistance = maxDistances["greenCyan"];
-      } else if(hue < okhslColorStops["blue"]) {
+        distance = Math.abs(okhslColorStops.green-hue);
+        maxDistance = maxDistances.greenCyan;
+      } else if(hue < okhslColorStops.blue) {
         // cyan-blue -> cyan
         dir = +1;
-        distance = Math.abs(okhslColorStops["blue"]-hue);
-        maxDistance = maxDistances["cyanBlue"];
-      } else if(hue < okhslColorStops["magenta"]) {
+        distance = Math.abs(okhslColorStops.blue-hue);
+        maxDistance = maxDistances.cyanBlue;
+      } else if(hue < okhslColorStops.magenta) {
         // blue-magenta -> magenta
         dir = -1;
-        distance = Math.abs(okhslColorStops["blue"]-hue);
-        maxDistance = maxDistances["blueMagenta"];
+        distance = Math.abs(okhslColorStops.blue-hue);
+        maxDistance = maxDistances.blueMagenta;
       } else {
         // magenta-red -> magenta (cont)
         dir = +1;
-        distance = Math.abs(360.0+okhslColorStops["red"]-hue);
-        maxDistance = maxDistances["magentaRed"];
+        distance = Math.abs(360.0+okhslColorStops.red-hue);
+        maxDistance = maxDistances.magentaRed;
       }
       
   }
   
-  return {dir: dir, distance: distance, maxDistance: maxDistance};
+  return {dir, distance, maxDistance};
 }
 
 /*************************
