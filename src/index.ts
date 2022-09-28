3627bcce3595f4be7313f91f53f1a43210d0b5eb
Janne Aukia <janne@robocorp.com>
2022-09-28 16:47:00 +0300

    Better handling of monochrome colors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 // tslint:disable-next-line: no-var-requires
-const { converter, formatHex } = require('../node_modules/culori/bundled/culori.min.cjs');
-// import { converter, formatHex } from 'culori';
+const { converter, formatHex } = require('../node_modules/culori/bundled/culori.cjs');
+//import { converter, formatHex } from 'culori';
+
+// sigh, require works on https://npm.runkit.com/superpal but not when using the
+// library locally.
 
 import { hsluvToHex, hexToHsluv } from 'hsluv';
 
@@ -168,11 +171,13 @@ const hexToHSL = (hexColor: string, colorSpaceIn: string) => {
   let inLig;
   if (colorSpaceIn === 'Okhsl') {
     const raw = OKHSL_CONVERTER(hexColor);
+    if(raw.h === undefined) raw.h = 0.0;
     [inHue, inSat, inLig] = [raw.h, raw.s * 100.0, raw.l * 100.0];
   } else if (colorSpaceIn === 'HSLuv') {
     [inHue, inSat, inLig] = hexToHsluv(hexColor);
   } else if (colorSpaceIn === 'HSL') {
     const raw = HSL_CONVERTER(hexColor);
+    if(raw.h === undefined) raw.h = 0.0;
     [inHue, inSat, inLig] = [raw.h, raw.s * 100.0, raw.l * 100.0];
   }
 
